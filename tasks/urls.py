8eefbdbc3d8525b26bab5d3548c1a5f699de828d
dmbak <dm.bakunin@gmail.com>
2022-06-29 20:24:49 +0300

    create a logout page

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from .views import LoginPageView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-	path('', views.index, name="list"),
-	path('update_task/<str:pk>/', views.updateTask, name="update_task"),
-	path('delete/<str:pk>/', views.deleteTask, name="delete"),
+    path('', views.index, name="list"),
+    path('login/', LoginPageView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,50 +1,61 @@
+from dataclasses import fields
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 
 from .models import *
 from .forms import *
 
+from django.contrib.auth.views import LoginView
 # Create your views here.
 
+
+class LoginPageView(LoginView):
+    template_name = 'tasks/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('list')
+
+
 def index(request):
-	tasks = Task.objects.all()
+    tasks = Task.objects.all()
 
-	form = TaskForm()
+    form = TaskForm()
 
-	if request.method =='POST':
-		form = TaskForm(request.POST)
-		if form.is_valid():
-			form.save()
-		return redirect('/')
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
 
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
 
-	context = {'tasks':tasks, 'form':form}
-	return render(request, 'tasks/list.html', context)
 
 def updateTask(request, pk):
-	task = Task.objects.get(id=pk)
+    task = Task.objects.get(id=pk)
 
-	form = TaskForm(instance=task)
+    form = TaskForm(instance=task)
 
-	if request.method == 'POST':
-		form = TaskForm(request.POST, instance=task)
-		if form.is_valid():
-			form.save()
-			return redirect('/')
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
 
-	context = {'form':form}
+    context = {'form': form}
 
-	return render(request, 'tasks/update_task.html', context)
+    return render(request, 'tasks/update_task.html', context)
+
 
 def deleteTask(request, pk):
-	item = Task.objects.get(id=pk)
+    item = Task.objects.get(id=pk)
 
-	if request.method == 'POST':
-		item.delete()
-		return redirect('/')
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
 
-	context = {'item':item}
-	return render(request, 'tasks/delete.html', context)
-
-
-
+    context = {'item': item}
+    return render(request, 'tasks/delete.html', context)
